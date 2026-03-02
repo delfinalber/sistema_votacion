95fc0362dd3cd863dee02d9a1fcf4305cd8be522
delfinalber <delfin.alber@gmail.com>
2026-03-02 14:22:43 -0500

    proyecto-votacione-02-03-2026

--- votaciones/DB/votaciones.sql
+++ votaciones/DB/votaciones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-03-2026 a las 18:44:38
+-- Tiempo de generación: 02-03-2026 a las 20:21:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -137,7 +137,67 @@ INSERT INTO `votantes` (`id_votante`, `nombre`, `voto_realizado`) VALUES
 ('12345', 'ALBER DELFIN PEÑA ORTIGOZA', 0),
 ('1234567891', 'pamela', 0),
 ('1586248598', 'Maria Paula', 0),
+('1627832407', 'Camila Navarro', 0),
+('1801207382', 'Felipe Ortega', 0),
+('1815446425', 'Diana Ortega', 0),
+('2132009779', 'Mateo Molina', 0),
+('2145882331', 'Mateo Molina', 0),
+('2403364610', 'Sergio Ruiz', 0),
+('2554534701', 'Isabella Méndez', 0),
+('2568472440', 'Isabella Navarro', 0),
+('2766834543', 'Ana Torres', 0),
+('2868759843', 'Sofía López', 0),
+('2953725146', 'Sofía Ortega', 0),
+('3284055678', 'Andrés Molina', 0),
+('3289738583', 'Valentina Ramírez', 0),
+('3482199578', 'María Ortega', 0),
+('3503268108', 'Paula Pérez', 0),
+('3777727578', 'Juan Méndez', 0),
+('3837178564', 'Valentina Herrera', 0),
+('3869735219', 'Sofía Pérez', 0),
+('3978080105', 'Luis Navarro', 0),
+('4169338792', 'Julián Silva', 0),
+('4376610225', 'Diana López', 0),
+('4604760086', 'Felipe Navarro', 0),
+('4625154099', 'Julián Ortega', 0),
+('4787653725', 'Nicolás Herrera', 0),
+('4859465485', 'María Gómez', 0),
+('5103207900', 'Laura Ramírez', 0),
+('5122179305', 'Ana León', 0),
+('5345175431', 'Mateo Pérez', 0),
+('5526157521', 'Daniel Rojas', 0),
+('5604738371', 'Juan Díaz', 0),
+('5713324141', 'Carlos Navarro', 0),
+('5803432566', 'Gabriela Torres', 0),
+('6011421995', 'Sofía Rojas', 0),
+('6105157807', 'María Ortega', 0),
+('6218193639', 'Ana Ramírez', 0),
+('6257692178', 'Ana Navarro', 0),
+('6459631200', 'Isabella Ortega', 0),
+('6473533062', 'Ana Ortega', 0),
+('6559845213', 'Sergio Ruiz', 0),
+('6773714765', 'Julián Pardo', 0),
+('6942744508', 'María Suárez', 0),
+('6989677841', 'Nicolás Méndez', 0),
+('7106159339', 'Julián Cortés', 0),
+('7260595311', 'Juan López', 0),
+('7264896482', 'Felipe Méndez', 0),
+('7307952399', 'Luis Pérez', 0),
 ('7722975', 'luz elena', 0),
+('7872576226', 'Diana León', 0),
+('8065864451', 'Ana León', 0),
+('8382290522', 'María Herrera', 0),
+('8768222997', 'Mateo Silva', 0),
+('8966271177', 'Carlos Rojas', 0),
+('9070488687', 'Camila Pardo', 0),
+('9401552242', 'Julián León', 0),
+('9455558327', 'Paula Díaz', 0),
+('9560448514', 'Diana Molina', 0),
+('9587023218', 'Laura Castro', 0),
+('9880846442', 'Diana Torres', 0),
+('9938542551', 'Julián Suárez', 0),
+('9948517215', 'Julián Pérez', 0),
+('9994670281', 'Nicolás Castro', 0),
 ('TST_RST2_20260302182454', 'TEST RESET2', 0),
 ('TST_RST_20260302182423', 'TEST RESET', 0);
 
@@ -155,6 +215,14 @@ CREATE TABLE `votos` (
   `valor_voto` int(1) NOT NULL,
   `fecha_voto` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `votos`
+--
+
+INSERT INTO `votos` (`id_voto`, `id_candidato`, `id_votante`, `es_blanco`, `valor_voto`, `fecha_voto`) VALUES
+(63, 0, '3284055678', 1, 1, '2026-03-02 18:57:51'),
+(64, 9, '3284055678', 0, 1, '2026-03-02 18:57:51');
 
 --
 -- Índices para tablas volcadas
@@ -200,7 +268,7 @@ ALTER TABLE `votos`
 -- AUTO_INCREMENT de la tabla `administrador`
 --
 ALTER TABLE `administrador`
-  MODIFY `id_administrador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_administrador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `candidatos`
@@ -218,7 +286,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `votos`
 --
 ALTER TABLE `votos`
-  MODIFY `id_voto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id_voto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
